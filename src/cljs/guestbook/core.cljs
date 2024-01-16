f94992ed268ff0478248b089eafaeb9925d4b545
Amere <ahmedamerxx@gmail.com>
2024-01-16 19:29:31 +0200

    Refactor client side and add per user posts page and routing

--- src/cljs/guestbook/core.cljs
+++ src/cljs/guestbook/core.cljs
@@ -1,450 +1,55 @@
 (ns guestbook.core
-  (:require [reagent.core :as r]
-            [reagent.dom :as dom]
-            [re-frame.core :as rf]
-            [clojure.string :as string]
-            [ajax.core :refer [GET POST]]
-            [guestbook.websockets :as ws]
-            [mount.core :as mount]
-            [guestbook.validation :refer [validate-message]]))
+  (:require
+   [reagent.core :as r]
+   [reagent.dom :as dom]
+   [re-frame.core :as rf]
+   [reitit.coercion.spec :as reitit-spec]
+   [reitit.frontend :as rtf]
+   [reitit.frontend.easy :as rtfe]
+   [reitit.frontend.controllers :as rtfc]
+   [clojure.string :as string]
+   [guestbook.routes.app :refer [app-routes]]
+   [guestbook.websockets :as ws]
+   [guestbook.auth :as auth]
+   [guestbook.messages :as messages]
+   [guestbook.ajax :as ajax]
+   [mount.core :as mount]))
 
-; Initialize app event
 (rf/reg-event-fx
  :app/initialize
  (fn [_ _]
-   {:db {:messages/loading? true
-         :session/loading? true}
-    :dispatch-n [[:session/load] [:messages/load]]}))
+   {:db       {:session/lonading? true}
+    :dispatch [:session/load]}))
 
-; Session handling
-(rf/reg-event-fx
- :session/load
- (fn [{:keys [db]} _]
-   {:db (assoc db :session/loading? true)
-    :ajax/get {:url "/api/session"
-               :success-path [:session]
-               :success-event [:session/set]}}))
+(def router
+  (rtf/router
+   (app-routes)
+   {:data {:coercion reitit-spec/coercion}}))
 
 (rf/reg-event-db
- :session/set
- (fn [db [_ {:keys [identity]}]]
-   (assoc db
-          :auth/user identity
-          :session/loading? false)))
+ :router/navigated
+ (fn [db [_ new-match]]
+   (assoc db :router/current-route new-match)))
 
 (rf/reg-sub
- :session/loading?
- (fn [db _]
-   (:session/loading? db)))
+ :router/current-route
+ (fn [db]
+   (:router/current-route db)))
 
-; Modal events
-(rf/reg-event-db
- :app/show-modal
- (fn [db [_ modal-id]]
-   (assoc-in db [:app/active-modals modal-id] true)))
+(defn init-routes! []
+  (rtfe/start!
+   router
+   (fn [new-match]
+     (when new-match
+       (let [{controllers :controllers}
+             @(rf/subscribe [:router/current-route])
+             new-match-with-controllers
+             (assoc new-match
+                    :controllers
+                    (rtfc/apply-controllers controllers new-match))]
+         (rf/dispatch [:router/navigated new-match-with-controllers]))))
+   {:use-fragment false}))
 
-(rf/reg-event-db
- :app/hide-modal
- (fn [db [_ modal-id]]
-   (update db :app/active-modals dissoc modal-id)))
-
-(rf/reg-sub
- :app/active-modals
- (fn [db _]
-   (:app/active-modals db {})))
-
-(rf/reg-sub
- :app/modal-showing?
- :<- [:app/active-modals]
- (fn [modals [_ modal-id]]
-   (get modals modal-id false)))
-
-(defn modal-card [id title body footer]
-  [:div.modal
-   {:class (when @(rf/subscribe [:app/modal-showing? id]) "is-active")}
-   [:div.modal-background
-    {:on-click #(rf/dispatch [:app/hide-modal id])}]
-   [:div.modal-card
-    [:header.modal-card-head
-     [:p.modal-card-title title]
-     [:button.delete
-      {:on-click #(rf/dispatch [:app/hide-modal id])}]]
-    [:section.modal-card-body
-     body]
-    [:footer.modal-card-foot
-     footer]]])
-
-(defn modal-button [id title body footer]
-  [:div
-   [:button.button.is-primary
-    {:on-click #(rf/dispatch [:app/show-modal id])}
-    title]
-   [modal-card id title body footer]])
-
-
-;; Login logic
-
-(rf/reg-event-db
- :auth/handle-login
- (fn [db [_ {:keys [identity]}]]
-   (assoc db :auth/user identity)))
-
-(rf/reg-event-db
- :auth/handle-logout
- (fn [db _]
-   (dissoc db :auth/user)))
-
-(rf/reg-sub
- :auth/user
- (fn [db _]
-   (:auth/user db)))
-
-(rf/reg-sub
- :auth/user-state
- :<-[:auth/user]
- :<-[:session/loading?]
- (fn [[user loading?]]
-   (cond
-     (true? loading?) :loading
-     user :authenticated
-     :else :anonymous)))
-
-(defn login-button []
-  (r/with-let
-      [fields (r/atom {})
-       error (r/atom nil)
-       do-login
-       (fn [_]
-         (reset! error nil)
-         (POST "/api/login"
-               {:headers {"Accept" "application/transit+json"}
-                :params @fields
-                :handler (fn [response]
-                           (reset! fields {})
-                           (rf/dispatch [:auth/handle-login response])
-                           (rf/dispatch [:app/hide-modal :user/login]))
-                :error-handler (fn [error-response]
-                                 (reset! error
-                                         (or
-                                          (:message (:response error-response))
-                                          (:status-text error-response)
-                                          "Unknown Error")))}))]
-    [modal-button :user/login
-     ;; Title
-     "Log In"
-     ;; Body
-     [:div
-      (when-not (string/blank? @error)
-        [:div.notification.is-danger
-         @error])
-      [:div.field
-       [:div.label "Login"]
-       [:div.control
-        [:input.input
-         {:type "text"
-          :value (:login @fields)
-          :on-change #(swap! fields assoc :login (.. % -target -value))}]]]
-      [:div.field
-       [:div.label "Password"]
-       [:div.control
-        [:input.input
-         {:type "password"
-          :value (:password @fields)
-          :on-change #(swap! fields assoc :password (.. % -target -value))
-          ;; Submit login form when `Enter` key is pressed
-          :on-key-down #(when (= (.-keyCode %) 13)
-                          (do-login))}]]]]
-     ;; Footer
-     [:button.button.is-primary.is-fullwidth
-      {:on-click do-login
-       :disabled (or (string/blank? (:login @fields))
-                     (string/blank? (:password @fields)))}
-      "Log In"]]))
-
-(defn logout-button []
-  [:button.button
-   {:on-click #(POST "/api/logout"
-                     {:handler (fn [_]
-                                 (rf/dispatch [:auth/handle-logout]))})}
-   "Log Out"])
-
-(defn nameplate [{:keys [login]}]
-  [:button.button.is-primary
-   login])
-
-(rf/reg-fx
- :ajax/get
- (fn [{:keys [url success-event error-event success-path]}]
-   (GET url
-        (cond-> {:headers {"Accept" "application/transit+json"}}
-          success-event (assoc :handler
-                               #(rf/dispatch
-                                 (conj success-event
-                                       (if success-path
-                                         (get-in % success-path)
-                                         %))))
-          error-event (assoc :error-handler
-                             #(rf/dispatch
-                               (conj error-event %)))))))
-
-;; Registeration
-(defn register-button []
-  (r/with-let
-      [fields (r/atom {})
-       error (r/atom nil)
-       do-register
-       (fn [_]
-         (reset! error nil)
-         (POST "/api/register"
-               {:headers {"Accept" "application/transit+json"}
-                :params @fields
-                :handler (fn [response]
-                           (reset! fields {})
-                           (rf/dispatch [:app/hide-modal :user/register])
-                           (rf/dispatch [:app/show-modal :user/login]))
-                :error-handler (fn [error-response]
-                                 (reset! error
-                                         (or
-                                          (:message (:response error-response))
-                                          (:status-text error-response)
-                                          "Unknown Error")))}))]
-    [modal-button :user/register
-     ;; Title
-     "Create Account"
-     ;; Body
-     [:div
-      (when-not (string/blank? @error)
-        [:div.notification.is-danger
-         @error])
-      [:div.field
-       [:div.label "Login"]
-       [:div.control
-        [:input.input
-         {:type "text"
-          :value (:login @fields)
-          :on-change #(swap! fields assoc :login (.. % -target -value))}]]]
-      [:div.field
-       [:div.label "Password"]
-       [:div.control
-        [:input.input
-         {:type "password"
-          :value (:password @fields)
-          :on-change #(swap! fields assoc :password (.. % -target -value))}]]]
-      [:div.field
-       [:div.label "Confirm Password"]
-       [:div.control
-        [:input.input
-         {:type "password"
-          :value (:confirm @fields)
-          :on-change #(swap! fields assoc :confirm (.. % -target -value))
-          ;; Submit login form when `Enter` key is pressed
-          :on-key-down #(when (= (.-keyCode %) 13)
-                          (do-register))}]]]]
-     ;; Footer
-     [:button.button.is-primary.is-fullwidth
-      {:on-click do-register
-       :disabled (or (string/blank? (:login @fields))
-                     (string/blank? (:password @fields))
-                     (string/blank? (:confirm @fields)))}
-      "Create Account"]]))
-
-(rf/reg-event-fx
- :messages/load
- (fn [{:keys [db]} _]
-   {:db (assoc db :messages/loading? true)
-    :ajax/get {:url "/api/messages"
-               :success-path [:messages]
-               :success-event [:messages/set]}}))
-
-(rf/reg-event-fx
- :message/send!-called-back
- (fn [_ [_ {:keys [success errors]}]]
-   (if success
-     {:dispatch [:form/clear-fields]}
-     {:dispatch [:form/set-server-errors errors]})))
-
-(rf/reg-event-fx
- :message/send!
- (fn [{:keys [db]} [_ fields]]
-   {:db (dissoc db :form/server-errors)
-    :ws/send! {:message [:message/create! fields]
-               :timeout 10000
-               :callback-event [:message/send!-called-back]}}))
-
-                                        ; Unwraps the db from the co-effects map, and wraps the return with {:db ...}
-                                        ; to be used only with events which use the db effect
-(rf/reg-event-db
- :messages/set
- (fn [db [_ messages]]
-   (-> db
-       (assoc :messages/loading? false
-              :messages/list messages))))
-
-(rf/reg-event-db
- :form/set-server-errors
- [(rf/path :form/server-errors)]
- (fn [_ [_ errors]]
-   errors))
-
-(rf/reg-event-db
- :message/add
- (fn [db [_ message]]
-   (update db :messages/list conj message)))
-
-(rf/reg-event-db
- :form/set-field
- [(rf/path :form/fields)]
- (fn [fields [_ id value]]
-   (assoc fields id value)))
-
-(rf/reg-event-db
- :form/clear-fields
- [(rf/path :form/fields)]
- (fn [_ _]
-   {}))
-
-(rf/reg-sub
- :form/server-errors
- (fn [db _]
-   (:form/server-errors db)))
-
-(rf/reg-sub
- :messages/list
- (fn [db _]
-   (:messages/list db [])))
-
-;;Validation errors are reactively computed
-(rf/reg-sub
- :form/validation-errors
- :<- [:form/fields]
- (fn [fields _]
-   (validate-message fields)))
-
-(rf/reg-sub
- :form/validation-errors?
- :<- [:form/validation-errors]
- (fn [errors _]
-   (not (empty? errors))))
-
-(rf/reg-sub
- :form/errors
- :<- [:form/validation-errors]
- :<- [:form/server-errors]
- (fn [[validation server] _]
-   (merge validation server)))
-
-(rf/reg-sub
- :form/error
- :<- [:form/errors]
- (fn [errors [_ id]]
-   (get errors id)))
-
-                                        ; Subscription to the :messages/loading?
-(rf/reg-sub
- :messages/loading?
- (fn [db _]
-   (:messages/loading? db)))
-
-(rf/reg-sub
- :form/fields
- (fn [db _]
-   (:form/fields db)))
-
-(rf/reg-sub
- :form/field
- :<- [:form/fields]
- (fn [fields [_ id]]
-   (get fields id)))
-
-
-
-(defn message-list [messages]
-  [:ul.messages
-   (for [{:keys [timestamp message name]} @messages]
-     ^{:key timestamp}
-     [:li
-      [:time (.toLocaleString timestamp)]
-      [:p message]
-      [:p "-" name]])])
-
-(defn errors-component [id]
-  (when-let [error @(rf/subscribe [:form/error id])]
-    [:div.notification.is-danger (string/join error)]))
-
-(defn text-input [{val :value
-                   attrs :attrs
-                   :keys [on-save]}]
-  (let [draft (r/atom nil)
-        value (r/track #(or @draft @val ""))]
-    (fn []
-      [:input.input
-       (merge attrs
-              {:type :text
-               :on-focus #(reset! draft (or @val ""))
-               :on-blur (fn []
-                          (on-save (or @draft ""))
-                          (reset! draft nil))
-               :on-change #(reset! draft (.. % -target -value))
-               :value @value})])))
-
-(defn textarea-input [{val :value
-                       attrs :attrs
-                       :keys [on-save]}]
-  (let [draft (r/atom nil)
-        value (r/track #(or @draft @val ""))]
-    (fn []
-      [:textarea.textarea
-       (merge attrs
-              {:on-focus #(reset! draft (or @val ""))
-               :on-blur (fn []
-                          (on-save (or @draft ""))
-                          (reset! draft nil))
-               :on-change #(reset! draft (.. % -target -value))
-               :value @value})])))
-
-(defn message-form []
-  [:div
-   [errors-component :server-error]
-   [:div.field
-    [:label.label {:for :name} "Name"]
-    [errors-component :name]
-    [text-input {:attrs {:name :name}
-                 :value (rf/subscribe [:form/field :name])
-                 :on-save #(rf/dispatch [:form/set-field :name %])}]]
-   [:div.field
-    [:label.label {:for :message} "Message"]
-    [errors-component :message]
-    [textarea-input
-     {:attrs {:name :message}
-      :value (rf/subscribe [:form/field :message])
-      :on-save #(rf/dispatch [:form/set-field :message %])}]]
-   [:input.button.is-primary
-    {:type :submit
-     :disabled @(rf/subscribe [:form/validation-errors?])
-     :on-click #(rf/dispatch [:message/send!
-                              @(rf/subscribe [:form/fields])])
-     :value "comment"}]])
-
-(defn reload-messages-button []
-  (let [loading? (rf/subscribe [:messages/loading?])]
-    [:button.button.is-info.is-fullwidth
-     {:on-click #(rf/dispatch [:messages/load])
-      :disabled @loading?}
-     (if @loading?
-       "Loading Messages"
-       "Refresh Messages")]))
-
-(defn home []
-  (let [messages (rf/subscribe [:messages/list])]
-    (fn []
-      [:div.content>div.columns.is-centered>div.column.is-two-thirds
-       [:div.columns>div.column
-        [:h3 "Messages"]
-        [message-list messages]]
-       [:div.columns>div.column
-        [reload-messages-button]]
-       [:div.columns>div.column
-        [message-form]]])))
 
 (defn navbar []
   (let [burger-active (r/atom false)]
@@ -453,13 +58,13 @@
        [:div.container
         [:div.navbar-brand
          [:a.navbar-item
-          {:href "/"
+          {:href  "/"
            :style {:font-weight "bold"}}
           "guestbook"]
          [:span.navbar-burger.burger
           {:data-target "nav-menu"
-           :on-click #(swap! burger-active not)
-           :class (when @burger-active "is-active")}
+           :on-click    #(swap! burger-active not)
+           :class       (when @burger-active "is-active")}
           [:span]
           [:span]
           [:span]]]
@@ -468,7 +73,12 @@
          [:div.navbar-start
           [:a.navbar-item
            {:href "/"}
-           "Home"]]
+           "Home"]
+          (when (= @(rf/subscribe [:auth/user-state]) :authenticated)
+            [:a.navbar-item
+             {:href (rtfe/href :guestbook.routes.app/author
+                               {:user (:login @(rf/subscribe [:auth/user]))})}
+             "My Posts"])]
          [:div.navbar-end
           [:div.navbar-item
            (case @(rf/subscribe [:auth/user-state])
@@ -477,28 +87,39 @@
               [:progress.progress.is-dark.is-small {:max 100} "30%"]]
              :authenticated
              [:div.buttons
-              [nameplate @(rf/subscribe [:auth/user])]
-              [logout-button]]
+              [auth/nameplate @(rf/subscribe [:auth/user])]
+              [auth/logout-button]]
              :anonymous
              [:div.buttons
-              [login-button]
-              [register-button]])]]]]])))
+              [auth/login-button]
+              [auth/register-button]])]]]]])))
+
+(defn page [{{:keys [view name]} :data
+             path                :path
+             :as                 match}]
+  [:section.section>div.container
+   (if view
+     [view match]
+     [:div "No view specified for route: " name " (" path ")"])])
 
 (defn app []
-  [:div.app
-   [navbar]
-   [:section.section
-    [:div.container
-     [home]]]])
+  (let [current-route @(rf/subscribe [:router/current-route])]
+    [:div.app
+     [navbar]
+     [page current-route]]))
 
 (defn ^:dev/after-load mount-components []
   (rf/clear-subscription-cache!)
   (.log js/console "Mounting Components...")
+  (init-routes!)
   (dom/render [#'app] (.getElementById js/document "content"))
   (.log js/console "Components Mounted..."))
 
 (defn init! []
   (.log js/console "Initializing App...")
   (mount/start)
-  (rf/dispatch [:app/initialize])
+  ;; To avoid race conditions we're going to dispatch async for initializtion,
+  ;; so it doesn't collide with load-messages-for-user if we refresh the page of a user
+  (rf/dispatch-sync [:app/initialize])
   (mount-components))
+
